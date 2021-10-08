88b602a9d49e05028d935cf002c219e83fd2d959
Cunning-Fox-99 <kostya.moroz.96@gmail.com>
2021-10-08 17:32:53 +0300

    fixes

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-const tasks = [
-  {
+const tasks = [{
     _id: "5d2ca9e2e03d40b326596aa7",
     completed: true,
     body: "Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n",
@@ -9,8 +8,7 @@ const tasks = [
     _id: "5d2ca9e29c8a94095c1288e0",
     completed: false,
     body: "Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n",
-    title:
-      "Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.",
+    title: "Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.",
   },
   {
     _id: "5d2ca9e2e03d40b3232496aa7",
@@ -22,13 +20,12 @@ const tasks = [
     _id: "5d2ca9e29c8a94095564788e0",
     completed: false,
     body: "Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n",
-    title:
-      "Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.",
+    title: "Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.",
   },
 ];
 
 (function (arrOfTasks) {
-  
+
   const objOfTasks = arrOfTasks.reduce((acc, task) => {
     acc[task._id] = task;
     return acc;
@@ -36,7 +33,7 @@ const tasks = [
 
   // Elements
 
-  const listContainer = document.querySelector ('.tasks-list-section .list-group');
+  const listContainer = document.querySelector('.tasks-list-section .list-group');
 
   const form = document.forms['addTask'];
   const inputTitle = form.elements['title'];
@@ -47,59 +44,63 @@ const tasks = [
   form.addEventListener('submit', onFormSubmitHandler);
   listContainer.addEventListener('click', onDeleteHandler);
 
-  
 
-  function renderAllTaska(tasksList){
+
+  function renderAllTaska(tasksList) {
     if (!tasksList) {
       console.error('Передайте список задач!');
       return;
     }
 
     const fragment = document.createDocumentFragment();
-    Object.values(tasksList).forEach(task =>{
-     const li = listItemTemplate(task);
-     fragment.appendChild(li);
+    Object.values(tasksList).forEach(task => {
+      const li = listItemTemplate(task);
+      fragment.appendChild(li);
     });
-  listContainer.appendChild (fragment);
+    listContainer.appendChild(fragment);
   }
 
-  function listItemTemplate ({ _id, title, body} = {}) {
+  function listItemTemplate({
+    _id,
+    title,
+    body
+  } = {}) {
     const li = document.createElement('li');
-    li.classList.add (
+    li.classList.add(
       'list-group-item',
-     'd-flex',
+      'd-flex',
       'align-items-center',
       'flex-wrap',
       'mt-2',
-      );
-      li.setAttribute('data-task-id', _id);
-      
-      const span = document.createElement('span');
-      span.textContent = title;
-      span.style.fontWeight = 'bold';
+    );
+    li.setAttribute('data-task-id', _id);
 
-      const deleteBtn = document.createElement('button');
-      deleteBtn.textContent = 'Delete task';
-      deleteBtn.classList.add('btn', 'btn-danger', 'ml-auto', 'delete-btn');
+    const span = document.createElement('span');
+    span.textContent = title;
+    span.style.fontWeight = 'bold';
 
-      const article = document.createElement('p');
-      article.textContent = body;
-      article.classList.add ('mt-2', 'w-100');
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete task';
+    deleteBtn.classList.add('btn', 'btn-danger', 'ml-auto', 'delete-btn');
 
-      li.appendChild(span);
-      li.appendChild(deleteBtn);
-      li.appendChild(article);
+    const article = document.createElement('p');
+    article.textContent = body;
+    article.classList.add('mt-2', 'w-100');
+
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+    li.appendChild(article);
 
     return li;
   }
 
-  function onFormSubmitHandler (e) {
+  function onFormSubmitHandler(e) {
     e.preventDefault();
     const titleValue = inputTitle.value;
     const bodyValue = inputBody.value;
-    
-    if (!titleValue || !bodyValue){
-      alert ('Пожалуйста введите title и body');
+
+    if (!titleValue || !bodyValue) {
+      alert('Пожалуйста введите title и body');
       return;
     }
 
@@ -109,40 +110,46 @@ const tasks = [
     form.reset();
   }
 
-  function createNewTask (title, body){
+  function createNewTask(title, body) {
 
-      const newTask = {
-        title,
-        body,
-        completed: false,
-        _id: `task-${Math.random()}`,
-      };
+    const newTask = {
+      title,
+      body,
+      completed: false,
+      _id: `task-${Math.random()}`,
+    };
 
-     objOfTasks[newTask._id] = newTask;
+    objOfTasks[newTask._id] = newTask;
 
-     return { ...newTask};
+    return {
+      ...newTask
+    };
   }
 
   function deleteTask(id) {
-    const { title } = objOfTasks[id];
-    const isConfirm = confirm (`Точно хотите удалить задачу: ${title}?`);
-    if(!isConfirm) return isConfirm;
+    const {
+      title
+    } = objOfTasks[id];
+    const isConfirm = confirm(`Точно хотите удалить задачу: ${title}?`);
+    if (!isConfirm) return isConfirm;
     delete objOfTasks[id];
     return isConfirm
-  }  
+  }
 
-function deleteTaskFromHtml (confirmed, el){
-  if (!confirmed) return;
-  el.remove();
-}
+  function deleteTaskFromHtml(confirmed, el) {
+    if (!confirmed) return;
+    el.remove();
+  }
 
-  function onDeleteHandler({target}) {
-    if (target.classList.contains('delete-btn')){
-    
-    const parent = target.closest('[data-task-id]');
-    const id = parent.dataset.taskId;
-    const confirmed = deleteTask(id);    
-    deleteTaskFromHtml(confirmed, parent);
+  function onDeleteHandler({
+    target
+  }) {
+    if (target.classList.contains('delete-btn')) {
+
+      const parent = target.closest('[data-task-id]');
+      const id = parent.dataset.taskId;
+      const confirmed = deleteTask(id);
+      deleteTaskFromHtml(confirmed, parent);
 
     }
   }
